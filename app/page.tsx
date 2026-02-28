18b9a9e2f2a7a7636e4b5236e9b68dd4348176c4
Abdul Rehamn <92205472+likeam@users.noreply.github.com>
2026-02-28 23:23:13 +0500

    Changing

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Home = () => {
-  return <div className=" bg-amber-600">Home Sweet too</div>;
+  return <div className=" bg-amber-600">Home Sweet too only</div>;
 };
 
 export default Home;
