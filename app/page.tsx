fa7831fa547ade5cbbd57e07f9d7865e17747a09
Abdul Rehamn <92205472+likeam@users.noreply.github.com>
2026-03-13 23:51:33 +0500

    Saving

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const Home = () => {
-  return <div className=" bg-amber-900">Home Sweet  too  has too only tun back</div>;
+  return (
+    <div className=" bg-amber-900">
+      Home Sweet too has been too only tun back
+    </div>
+  );
 };
 
 export default Home;
