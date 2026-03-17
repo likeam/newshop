8da67834bd9968c96458d3e1915f35ed04cf61bc
Abdul Rehamn <92205472+likeam@users.noreply.github.com>
2026-03-17 22:50:00 +0500

    Settings

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import React from "react";
 const Home = () => {
   return (
     <div className=" bg-amber-900">
-      Home Sweet too has been too only tun me Settings back needed
+      Home Sweet too has been too only tun me Settings needed back needed
     </div>
   );
 };
