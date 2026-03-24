8163af4b3633859c8a7e87557914dd6b060bfe73
Abdul Rehamn <92205472+likeam@users.noreply.github.com>
2026-03-24 23:04:30 +0500

    Changing

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,9 @@ import React from "react";
 
 const Home = () => {
   return (
-    <div className=" bg-amber-800">
-      Home Sweet too has been too only seems under tun me Settings needed back needed
+    <div className=" bg-amber-700">
+      Home Sweet too has been too only seems under tun me Settings needed back
+      needed
     </div>
   );
 };
